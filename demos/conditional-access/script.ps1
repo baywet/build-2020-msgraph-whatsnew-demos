fca1ff10fd0ab3f7f1ef38ec4a7c96be67963526
Vincent Biret <vincentbiret@hotmail.com>
2020-05-04 12:38:39 -0400

    - fixes conditional access demo to work with PS7

--- demos/conditional-access/script.ps1
+++ demos/conditional-access/script.ps1
@@ -1,12 +1,31 @@
 Import-Module Microsoft.Graph
 Connect-Graph -Scopes "Policy.ReadWrite.ConditionalAccess", "Application.Read.All", "Policy.Read.All"
+
 Get-MgConditionalAccessPolicy
+
 Read-Host -Prompt "We just: 1. Loaded MS Graph PS, 2. Connected with required permissions, 3. Listed out all existing policies, Press enter key to continue."
+
+$conditions = @{
+    applications = @{
+        includeApplications = @("00000002-0000-0ff1-ce00-000000000000");
+    };
+    users = @{
+        includeUsers = @("All");
+    };
+    clientAppTypes = @("browser", "modern");
+    locations = @{
+        includeLocations = @("all");
+    }
+}
+
 New-MgConditionalAccessPolicy -DisplayName "MFA required when using Exchange"
     -GrantControlBuiltInControls @('mfa')
     -State 'enabled'
     -GrantControlOperator 'OR'
-    -UserIncludeUsers 'All'
-    -ApplicationIncludeApplications '00000002-0000-0ff1-ce00-000000000000'
+    -Conditions $conditions
+
 Get-MgConditionalAccessPolicy
+
 Read-Host -Prompt "We just: 1. Created a new policy, 2. Listed out all existing policies, Press enter key to finish."
+
+Disconnect-Graph
